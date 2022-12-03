1e0640a84929990165f014cf9af78090a432f3b1
kitdim <kitaykin9808@gmail.com>
2022-12-03 11:08:45 +0300

    Начал реализовывать интерфейс ISmoke в классе Smoke

--- KillSmokeConsoleaApp/KillSmokeConsoleaApp/Smoke.cs
+++ KillSmokeConsoleaApp/KillSmokeConsoleaApp/Smoke.cs
@@ -1,13 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using KillSmokeConsoleaApp.Interfaces;
 
 namespace KillSmokeConsoleaApp
 {
-    internal class Smoke
+    internal class Smoke : ISmoking
     {
+        public int CountСigarettуte { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int CountDateWithoutСigarettуte { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int CountSmokingСigarettуte { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public Smoke()
+        {
+        }
 
+        public void ShowSmokeInfo()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
