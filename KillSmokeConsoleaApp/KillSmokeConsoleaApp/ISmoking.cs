dc5f768ebe2d926c7469ac3bc18e10a152173f4a
kitdim <kitaykin9808@gmail.com>
2022-12-03 10:20:56 +0300

    Добавил интерфейс который будет описывать поведение класса Smoke

--- KillSmokeConsoleaApp/KillSmokeConsoleaApp/ISmoking.cs
+++ KillSmokeConsoleaApp/KillSmokeConsoleaApp/ISmoking.cs
@@ -2,5 +2,12 @@
 {
     internal interface ISmoking
     {
+        int CountСigarettуte { get; set; }
+        int CountDateWithoutСigarettуte { get; set; }
+        int CountSmokingСigarettуte { get; set; }
+
+        void ShowSmokeInfo();
+
+
     }
 }
