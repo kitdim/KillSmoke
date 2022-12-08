4be2060c7ce3733ccd4a5d572ce7e20c2a400810
kitdim <kitaykin9808@gmail.com>
2022-12-08 22:57:18 +0300

    Описал интерфейс для сохранения

--- KillSmokeConsoleaApp/KillSmokeConsoleaApp/Interfaces/ISave.cs
+++ KillSmokeConsoleaApp/KillSmokeConsoleaApp/Interfaces/ISave.cs
@@ -2,6 +2,8 @@
 {
     internal interface ISave
     {
-
+        static void SaveIsSmoke() { }
+        static void SaveCountSmokingСigarettуte() { }
+        static void SaveDaysWithoutСigarettуte() { }
     }
 }
