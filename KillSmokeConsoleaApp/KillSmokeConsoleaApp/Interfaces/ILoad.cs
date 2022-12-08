3b051e3ceab46d33429c022cc99e7da10284fdb3
kitdim <kitaykin9808@gmail.com>
2022-12-08 22:57:03 +0300

    Описал интерфейс для загрузки

---
+++ KillSmokeConsoleaApp/KillSmokeConsoleaApp/Interfaces/ILoad.cs
@@ -0,0 +1,9 @@
+﻿namespace KillSmokeConsoleaApp.Interfaces
+{
+    internal interface ILoad
+    {
+        static void LoadIsSmoke() { }
+        static void LoadCountSmokingСigarettуte() { }
+        static void LoadDaysWithoutСigarettуte() { }
+    }
+}
