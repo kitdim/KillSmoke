d201fc49150210f4866970e01d02f8bfe6bcfc4c
kitdim <kitaykin9808@gmail.com>
2022-12-10 17:03:08 +0300

    Добавил поля экземпляров классов Save и Load

--- KillSmokeConsoleaApp/KillSmokeConsoleaApp/Models/Smoke.cs
+++ KillSmokeConsoleaApp/KillSmokeConsoleaApp/Models/Smoke.cs
@@ -4,12 +4,15 @@ namespace KillSmokeConsoleaApp.Models
 {
     internal class Smoke : ISmoking
     {
+        private Load load;
+        private Save save;
         public int CountDaysWithoutСigarettуte { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
         public int CountSmokingСigarettуte { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
         public bool IsSmoke { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
 
         public Smoke()
         {
+            
         }
 
         public void ShowSmokeInfo()
