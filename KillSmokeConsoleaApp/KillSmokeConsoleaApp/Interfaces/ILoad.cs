5707928b34d60c9d8dcf09f3a49c22447577006c
kitdim <kitaykin9808@gmail.com>
2022-12-17 19:31:50 +0300

    Удалил все интерфейсы

--- KillSmokeConsoleaApp/KillSmokeConsoleaApp/Interfaces/ILoad.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace KillSmokeConsoleaApp.Interfaces
-{
-    internal interface ILoad
-    {
-        static void LoadIsSmoke() { }
-        static void LoadCountSmokingСigarettуte() { }
-        static void LoadDaysWithoutСigarettуte() { }
-    }
-}
--- KillSmokeConsoleaApp/KillSmokeConsoleaApp/Interfaces/ISave.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace KillSmokeConsoleaApp.Interfaces
-{
-    internal interface ISave
-    {
-        static void SaveIsSmoke() { }
-        static void SaveCountSmokingСigarettуte() { }
-        static void SaveDaysWithoutСigarettуte() { }
-    }
-}
