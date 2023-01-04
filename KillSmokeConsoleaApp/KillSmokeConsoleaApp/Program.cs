a21f9517fdaf7af458b630edf3cbd28d47919293
kitdim <kitaykin9808@gmail.com>
2023-01-04 18:51:47 +0300

    Мелкие правки

--- KillSmokeConsoleaApp/KillSmokeConsoleaApp/Program.cs
+++ KillSmokeConsoleaApp/KillSmokeConsoleaApp/Program.cs
@@ -1,11 +1,11 @@
 ﻿using KillSmokeConsoleaApp.Main;
 
-
-Console.WriteLine("The KillSmoke shows the number of days without cigarettes.\n\n");
-
+Console.WriteLine("The KillSmoke shows the number of days without cigarettes.\n");
 Console.WriteLine("Statistics for today:");
 
-Console.Write("\n\nDid you smoke today?(+|-): ");
+Console.Write("\n\nDid you smoke today?(Yes|Not): ");
+var answer = Console.ReadLine();
+
 
 
 Console.ReadLine();
