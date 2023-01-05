a60f61983b618f8d754ad71422590776b82e8356
kitdim <kitaykin9808@gmail.com>
2023-01-05 19:15:09 +0300

    Добавил свитч для выбора да/нет Добавил метод для загрузки данных

--- KillSmokeConsoleaApp/KillSmokeConsoleaApp/Program.cs
+++ KillSmokeConsoleaApp/KillSmokeConsoleaApp/Program.cs
@@ -4,8 +4,27 @@ Console.WriteLine("The KillSmoke shows the number of days without cigarettes.\n"
 Console.WriteLine("Statistics for today:");
 
 Console.Write("\n\nDid you smoke today?(Yes|Not): ");
-var answer = Console.ReadLine();
+var answer = Console.ReadLine().ToLower();
 
+switch (answer)
+{
+    case "yes":
+        
+        break;
+    case "not":
+        SetSmoke();
+        break;
+    default:
+        Console.WriteLine("Error");
+        break;
+}
 
+void SetSmoke()
+{
+    var countCig = Load.Loading();
+    var myCig = new Smoke(countCig);
+
+    myCig.ShowSmokeInfo();
+}
 
 Console.ReadLine();
