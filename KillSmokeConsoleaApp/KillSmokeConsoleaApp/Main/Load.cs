2247bf00ebfd8efa043a648a6f4ecf64545a3a1d
kitdim <kitaykin9808@gmail.com>
2023-01-05 19:14:09 +0300

    Добавил загрузка, но пока не работает

---
+++ KillSmokeConsoleaApp/KillSmokeConsoleaApp/Main/Load.cs
@@ -0,0 +1,16 @@
+﻿namespace KillSmokeConsoleaApp.Main
+{
+    internal static class Load
+    {
+        private const string PATH = @"data.dat";
+        public static int Loading()
+        {
+            using (var reader = new BinaryReader(File.Open(PATH, FileMode.OpenOrCreate)))
+            {
+                var countCig = reader.ReadInt32();
+                return countCig;
+            }
+        }
+
+    }
+}
