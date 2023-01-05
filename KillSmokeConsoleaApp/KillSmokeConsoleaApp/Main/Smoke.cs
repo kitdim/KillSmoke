d16cfb3a555404fa2e57154e5944d2024d01a502
kitdim <kitaykin9808@gmail.com>
2023-01-05 19:14:23 +0300

    Убрал количество дней

--- KillSmokeConsoleaApp/KillSmokeConsoleaApp/Main/Smoke.cs
+++ KillSmokeConsoleaApp/KillSmokeConsoleaApp/Main/Smoke.cs
@@ -3,28 +3,18 @@
     internal class Smoke
     {
         private int _countDays;
-        private int _countCigarars;
-        private Autoload autoload = new Autoload();
-        public Smoke(int countDays, int countCigar)
+        public Smoke(int countDays)
         {
             CountDaysWithoutСigarettуte = countDays;
-            CountSmokingСigarettуte = countCigar;
         }
         public int CountDaysWithoutСigarettуte
         {
             get { return _countDays; }
             private set { _countDays = value; }
         }
-        public int CountSmokingСigarettуte 
-        {   
-            get { return _countCigarars; }
-            private set { _countCigarars = value; }    
-        }
-
         public void ShowSmokeInfo()
         {
             Console.WriteLine("Days without cigarettes: {0}", CountDaysWithoutСigarettуte);
-            Console.WriteLine("Smoked cigarettes: {0}", CountSmokingСigarettуte);
         }
     }
 }
