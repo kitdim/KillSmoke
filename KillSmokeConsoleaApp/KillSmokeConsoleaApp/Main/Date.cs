1a80bae222d53369e3750e2cbc0789f7bd2d1f03
kitdim <kitaykin9808@gmail.com>
2023-01-28 01:39:27 +0300

    Добавил класс в котором будет описана логика проверки даты

---
+++ KillSmokeConsoleaApp/KillSmokeConsoleaApp/Main/Date.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KillSmokeConsoleaApp.Main
+{
+    internal class Date
+    {
+    }
+}
